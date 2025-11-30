5303202cef5d05a9272f55cf98fe7ff3258cce26
arif <arifk62901@gmail.com>
2025-11-30 18:35:24 +0600

    add post and get and update post particular user

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const initDB = async () => {
         description VARCHAR(50) NOT NULL,
         completed BOOLEAN DEFAULT FALSE,
         due_date DATE,
-      CREATED_AT TIMESTAMP  DEFAULT NOW(),
+        CREATED_AT TIMESTAMP  DEFAULT NOW(),
         UPDATED_AT TIMESTAMP   DEFAULT NOW()
       )`);
     console.log("Database tables initialize");
@@ -100,12 +100,13 @@ app.post("/add-user", async (req: Request, res: Response) => {
 app.get("/get-all-user", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`
-      SELECT * FROM users ORDER BY CREATED_AT DESC
+      SELECT * FROM users ORDER BY CREATED_AT ASC
       `);
-    res.status(400).json({
+    res.status(200).json({
       success: true,
       count: result.rowCount,
-      message: result.rows,
+      message: "All user data",
+      user: result.rows,
     });
   } catch (error) {
     console.log(error);
@@ -116,7 +117,7 @@ app.get("/get-all-user", async (req: Request, res: Response) => {
   }
 });
 
-// post a todo
+// * post a todo
 
 app.post("/add-toDo", async (req: Request, res: Response) => {
   try {
@@ -138,6 +139,96 @@ app.post("/add-toDo", async (req: Request, res: Response) => {
     res.status(400).json({
       success: false,
       message: "To do add something went wrong",
+    });
+  }
+});
+// * get all todo
+app.get("/show-all-toDo", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM toDo`);
+    res.status(200).json({
+      success: true,
+      count: result.rowCount,
+      message: "All TODO DATA ",
+      todo: result.rows,
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      success: false,
+      message: "fetch all user data",
+    });
+  }
+});
+
+//* single todo get
+app.get("/users/:id/todo", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; // 1
+
+    if (isNaN(Number(id))) {
+      res.status(400).json({
+        message: "please provide a valide user id",
+      });
+    }
+    const todoResult = await pool.query(
+      `SELECT * FROM toDo where user_id = $1`,
+      [id]
+    );
+    if (todoResult.rowCount === 0) {
+      res.status(404).json({
+        message: "todo not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user: todoResult.rows,
+    });
+    console.log(todoResult.rows);
+  } catch (error: any) {
+    console.log(error?.message);
+    res.status(400).json({
+      success: false,
+      message: "Not found todo",
+    });
+  }
+});
+
+// * update particular  user todo  data
+
+app.put("/users/:userId/todos/:todoId", async (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+    const { userId, todoId } = req.params; // 1
+
+    if (isNaN(Number(userId)) || isNaN(Number(todoId))) {
+      return res.status(400).json({
+        message: "please provide a valide user id",
+      });
+    }
+    const singleUserUpdate = await pool.query(
+      `UPDATE toDo SET title=$1, description=$2 where id=$3 and user_id=$4 RETURNING *`,
+      [title, description, todoId, userId]
+    );
+    if (singleUserUpdate.rowCount === 0) {
+      return res.status(404).json({
+        message: "todo not found",
+      });
+    }
+    console.log("update todo -> ", singleUserUpdate.rows[0]);
+    res.status(200).json({
+      success: true,
+      message: "Update TODO successfully",
+      user: singleUserUpdate.rows[0],
+    });
+  } catch (error: any) {
+    console.log(error?.message);
+    res.status(400).json({
+      success: false,
+      error: error?.message,
+      message: "Not found TODO",
     });
   }
 });
@@ -170,6 +261,77 @@ app.get("/user/:id", async (req: Request, res: Response) => {
     res.status(400).json({
       success: false,
       message: "Not found user",
+    });
+  }
+});
+
+// update user data
+
+app.patch("/update-user/:id", async (req: Request, res: Response) => {
+  try {
+    const { name, email, age, phone, address } = req.body;
+    const { id } = req.params; // 1
+
+    if (isNaN(Number(id))) {
+      res.status(400).json({
+        message: "please provide a valide user id",
+      });
+    }
+    const singleUserUpdate = await pool.query(
+      `UPDATE users SET name=$1, email=$2, age=$3, phone=$4 ,address=$5 where id=$6 RETURNING*`,
+      [name, email, age, phone, address, id]
+    );
+    if (singleUserUpdate.rowCount === 0) {
+      res.status(404).json({
+        message: "user not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Update user successfully",
+      user: singleUserUpdate.rows[0],
+    });
+  } catch (error: any) {
+    console.log(error?.message);
+    res.status(400).json({
+      success: false,
+      error: error?.message,
+      message: "Not found user",
+    });
+  }
+});
+
+// delete single user data
+app.delete("/user-delete/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; // 1
+
+    if (isNaN(Number(id))) {
+      res.status(400).json({
+        message: "please provide a valide user id",
+      });
+    }
+    const singleUser = await pool.query(`DELETE FROM users where id = $1`, [
+      id,
+    ]);
+    if (singleUser.rowCount === 0) {
+      res.status(404).json({
+        message: "user not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user: singleUser.rowCount,
+      userdata: singleUser.rows,
+      message: "Successfully delete",
+    });
+  } catch (error: any) {
+    console.log(error?.message);
+    res.status(400).json({
+      success: false,
+      message: "something went wrong",
     });
   }
 });
